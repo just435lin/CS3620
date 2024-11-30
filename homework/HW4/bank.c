fced2a67ea3734be6de17955ae706efaefc26707
JustinL <just435lin@gmail.com>
2024-11-30 09:49:14 -0600

    started on bank.c

--- homework/HW4/bank.c
+++ homework/HW4/bank.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bank_interface.h"
+#include <stdbool.h>
 
 typedef struct account {
     unsigned int account;
@@ -13,18 +14,72 @@ typedef struct ledger {
     account_t *tail;
 } ledger_t;
 
+account_t* contains_account(unsigned int account, void *ledger) {
+    ledger_t *bank = ledger;
+    if (bank->head == NULL) { return NULL; } //zero accounts exists
+    if (bank->head == bank->tail) { //only one account exist
+        if (bank->head->account == account) {
+            return bank->head;
+        } else {
+            return NULL;
+        }
+    }
+    account_t *curr = bank->head;
+    while (curr->next != NULL) {
+        if (curr->account == account) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
 int create_account(unsigned int account, void *ledger) {
     // TODO:
+    ledger_t *bank = ledger;
+    if (bank->head == NULL) {
+        bank->head = &account;
+        bank->tail = &account;
+    } else {
+        if (contains_account(account, ledger) == NULL) {
+            account_t *new_account = (account_t*)malloc(sizeof(account_t));
+            new_account->account = account;
+            new_account->balance = 0;
+            new_account->next = NULL;
+            bank->tail->next = new_account;
+            bank->tail = new_account;
+        }
+    }
+
     return -1;
 }
 
 void list_accounts(void *ledger) {
     // TODO:
+    ledger_t *bank = ledger;
+    if (bank->head == NULL) { //zero accounts exists
+        printf("No accounts have been created\n");
+        return; 
+    } 
+    if (bank->head == bank->tail) { //only one account exist
+        unsigned int acc_num = bank->head->account;
+        int acc_bal = bank->head->balance;
+        printf("WIP");
+    }
+
+    account_t *curr = bank->head;
+    while (curr->next != NULL) {
+        printf("WIP");
+        curr = curr->next;
+    }
 }
 
 int modify_balance(unsigned int account, int balance, void *ledger) {
     // TODO:
-
+    account_t* acc = contains_account(account, ledger);
+    if (acc != NULL) {
+        acc->balance = acc->balance + balance;
+    }
     return -1;
 }
 
